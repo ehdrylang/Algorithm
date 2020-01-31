bbc80649deb661cc7041b27a52153645d9090e4e
Dong gyo Jeong <ehdrylang@naver.com>
2020-01-31 21:30:27 +0900

    init project

---
+++ src/main/java/com/tistory/jeongpro/JeongproApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.jeongpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeongproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeongproApplication.class, args);
+	}
+
+}
