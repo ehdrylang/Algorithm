1f246a15feca8ff9e8f63dc5de6452437a43a3bd
Dong gyo Jeong <ehdrylang@naver.com>
2020-02-09 20:33:50 +0900

    백준 2*1 tile 문제

--- src/main/java/com/tistory/jeongpro/algorithm/tile.java
+++ src/main/java/com/tistory/jeongpro/algorithm/tile.java
@@ -1,5 +1,7 @@
 package com.tistory.jeongpro.algorithm;
 
+import java.util.Scanner;
+
 /**
  * 2×n 크기의 직사각형을 1×2, 2×1 타일로 채우는 방법의 수를 구하는 프로그램을 작성하시오.
  *
@@ -17,4 +19,33 @@ package com.tistory.jeongpro.algorithm;
  * 1부터 점화식을 찾아보자.
  */
 public class tile {
+    private static int[] results = new int[1001];
+    public static int 경우의수(){
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine()); // 2*n
+        //1
+        results[0] = 0;
+        results[1] = 1;
+        results[2] = 2;
+        for(int i=3;i<=n;i++){
+            results[i] = results[i-1] + results[i-2];
+        }
+    /*import java.util.Scanner;
+public class Main{
+    private static int[] results = new int[1001];
+    public static void main(String[] args){
+        results[0]=0;
+        results[1]=1;
+        results[2]=2;
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i=3;i<1001;i++){
+            results[i] = results[i-1] + results[i-2];
+        }
+        System.out.print(results[n]%10007);
+    }
+}*/
+
+        return n;
+    }
 }
