f4489b172f023de013e7c529054360445bce59a0
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-16 23:36:44 +0900

    top문제

---
+++ src/main/java/com/tistory/jeongpro/algorithm/Top.java
@@ -0,0 +1,18 @@
+package com.tistory.jeongpro.algorithm;
+
+import java.util.Stack;
+
+/**
+ * 프로그래머스 '탑' 문제
+ * 송수신하는 탑
+ */
+public class Top {
+    public int[] solution(int[] heights) {
+        int[] answer = {};
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i=0;i<heights.length;i++){
+            answer[i]
+        }
+        return answer;
+    }
+}
