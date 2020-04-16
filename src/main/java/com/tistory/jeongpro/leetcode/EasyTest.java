eeda7ddb28e8fbab03a3014db6ae71425fcca86d
Dong gyo Jeong <ehdrylang@naver.com>
2020-04-16 20:06:47 +0900

    string compress

--- src/main/java/com/tistory/jeongpro/leetcode/EasyTest.java
+++ src/main/java/com/tistory/jeongpro/leetcode/EasyTest.java
@@ -1,5 +1,8 @@
 package com.tistory.jeongpro.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EasyTest {
     public int numJewelsInStones(String J, String S) {
         int count = 0;
@@ -11,5 +14,18 @@ public class EasyTest {
             }
         }
         return count;
+    }
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<nums.length;i+=2){
+            for(int j=0;j<nums[i];j++){
+                list.add(nums[i+1]);
+            }
+        }
+        int[] result = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
