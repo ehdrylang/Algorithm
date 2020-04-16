79f71726a6e72fc5b62f50b9a6787f3fb0836ebd
Dong gyo Jeong <ehdrylang@naver.com>
2020-04-16 20:14:12 +0900

    자릿수 곱한거랑 더한거랑 빼기

--- src/main/java/com/tistory/jeongpro/leetcode/EasyTest.java
+++ src/main/java/com/tistory/jeongpro/leetcode/EasyTest.java
@@ -28,4 +28,15 @@ public class EasyTest {
         }
         return result;
     }
+    public int subtractProductAndSum(int n) {
+        int mul = 1;
+        int sum = 0;
+        while(n > 0){
+            int value = n % 10;
+            mul *= value;
+            sum += value;
+            n /= 10;
+        }
+        return mul - sum;
+    }
 }
