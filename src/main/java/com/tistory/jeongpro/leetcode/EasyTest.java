c7e96a311030e23157a54b4bfe4bef438dc8cc24
ehdrylang <ehdrylang@naver.com>
2020-04-17 17:41:33 +0900

    정점 최단거리찾기

--- src/main/java/com/tistory/jeongpro/leetcode/EasyTest.java
+++ src/main/java/com/tistory/jeongpro/leetcode/EasyTest.java
@@ -125,4 +125,11 @@ public class EasyTest {
         }
         return sum;
     }
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int steps = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            steps += Math.max(Math.abs(points[i][1] - points[i + 1][1]), Math.abs(points[i][0] - points[i + 1][0]));
+        }
+        return steps;
+    }
 }
