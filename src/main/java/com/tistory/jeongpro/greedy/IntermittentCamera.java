d08470ea878fceb12be392471d79c3e14a227709
ehdrylang <ehdrylang@naver.com>
2020-05-20 15:28:22 +0900

    고속도로 카메라
    겹치는 부분을 체크하는 로직이 어려웠다...
    아직 이해가 덜 된 것 같아 조금더 확인해봐야한다.

---
+++ src/main/java/com/tistory/jeongpro/greedy/IntermittentCamera.java
@@ -0,0 +1,45 @@
+package com.tistory.jeongpro.greedy;
+
+import java.util.Arrays;
+
+/**
+ * 단속카메라
+ * 문제 설명
+ * 고속도로를 이동하는 모든 차량이 고속도로를 이용하면서 단속용 카메라를 한 번은 만나도록 카메라를 설치하려고 합니다.
+ *
+ * 고속도로를 이동하는 차량의 경로 routes가 매개변수로 주어질 때, 모든 차량이 한 번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치해야 하는지를 return 하도록 solution 함수를 완성하세요.
+ *
+ * 제한사항
+ *
+ * 차량의 대수는 1대 이상 10,000대 이하입니다.
+ * routes에는 차량의 이동 경로가 포함되어 있으며 routes[i][0]에는 i번째 차량이 고속도로에 진입한 지점, routes[i][1]에는 i번째 차량이 고속도로에서 나간 지점이 적혀 있습니다.
+ * 차량의 진입/진출 지점에 카메라가 설치되어 있어도 카메라를 만난것으로 간주합니다.
+ * 차량의 진입 지점, 진출 지점은 -30,000 이상 30,000 이하입니다.
+ * 입출력 예
+ *
+ * routes	                                return
+ * [[-20,15], [-14,-5], [-18,-13], [-5,-3]]	2
+ * 입출력 예 설명
+ *
+ * -5 지점에 카메라를 설치하면 두 번째, 네 번째 차량이 카메라를 만납니다.
+ *
+ * -15 지점에 카메라를 설치하면 첫 번째, 세 번째 차량이 카메라를 만납니다.
+ */
+public class IntermittentCamera {
+
+    public int solution(int[][] routes) {
+        int answer = 1;
+        Arrays.sort(routes, (a,b)->a[0]-b[0]);
+        int temp = routes[0][1];
+        for(int i=0;i<routes.length-1;i++){
+            if(temp > routes[i][1]){
+                temp = routes[i][1];
+            }
+            if(temp < routes[i+1][0]){
+                answer++;
+                temp = routes[i+1][1];
+            }
+        }
+        return answer;
+    }
+}
