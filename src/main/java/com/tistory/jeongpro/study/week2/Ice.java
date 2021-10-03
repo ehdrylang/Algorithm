c7528c8bf42011283df819a80f51f64b040b75f6
dkjeong <ehdrylang@naver.com>
2021-10-03 13:25:06 +0900

    얼음 블록단위 세기 해결

---
+++ src/main/java/com/tistory/jeongpro/study/week2/Ice.java
@@ -0,0 +1,38 @@
+package com.tistory.jeongpro.study.week2;
+
+/**
+ * 얼음 얼려 먹기
+ * 얼음 덩어리 단위 개수 세기
+ * 00
+ * 00
+ * 00
+ * 00
+ */
+public class Ice {
+    public static int solution(int n, int m, int[][] iceBlocks) {
+        int result = 0;
+
+        int[][] visited = new int[n][m];
+
+        for (int i = 0; i < iceBlocks.length; i++) {
+            for (int j = 0; j < iceBlocks[i].length; j++) {
+                if (iceBlocks[i][j] == 0) {
+                    if (visited[i][j] == 0) {
+                        result++;
+                        visited[i][j] = result;
+                    }
+                    int k = i + 1;
+                    if (k < n && iceBlocks[k][j] == 0) {
+                        visited[k][j] = result;
+                    }
+                    int l = j + 1;
+                    if (l < m && iceBlocks[i][l] == 0) {
+                        visited[i][l] = result;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
