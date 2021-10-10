0089488b8d0248f7cf6d41ed7451f05eff674016
dkjeong <ehdrylang@naver.com>
2021-10-10 15:14:56 +0900

    binary search 추가

--- src/main/java/com/tistory/jeongpro/study/week3/ComponentSearch.java
+++ src/main/java/com/tistory/jeongpro/study/week3/ComponentSearch.java
@@ -1,5 +1,6 @@
 package com.tistory.jeongpro.study.week3;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,5 +15,37 @@ public class ComponentSearch {
         return wants.stream()
                 .map(i -> stocks.contains(i) ? "yes" : "no")
                 .collect(Collectors.joining(" "));
+    }
+
+    public static String solution2(List<Integer> components, int n, List<Integer> wants, int m) {
+        Collections.sort(components);
+        StringBuilder sb = new StringBuilder();
+        for (Integer value : wants) {
+            boolean exist = search(components, 0, components.size() - 1, value);
+            if (exist) {
+                sb.append("yes ");
+            } else {
+                sb.append("no ");
+            }
+        }
+        return sb.toString().trim();
+    }
+    // 1,2,3,4,5   0, 4
+    // 1,3,4,2     0, 3
+    private static boolean search(List<Integer> list, int startIdx, int endIndex, int searchValue) {
+        if(startIdx <= endIndex) {
+            int midIndex = (startIdx + endIndex) / 2;
+            int midValue = list.get(midIndex);
+            if (midValue == searchValue) {
+                return true;
+            }
+            if(midValue < searchValue) {
+                startIdx = midIndex + 1;
+            } else {
+                endIndex = midIndex - 1;
+            }
+            return search(list, startIdx, endIndex, searchValue);
+        }
+        return false;
     }
 }
--- src/test/java/com/tistory/jeongpro/study/week3/ComponentSearchTest.java
+++ src/test/java/com/tistory/jeongpro/study/week3/ComponentSearchTest.java
@@ -16,6 +16,26 @@ class ComponentSearchTest {
         List<Integer> list2 = new ArrayList<>(Arrays.asList(5,7,9));
         Assertions.assertThat(ComponentSearch.solution(list1, 5, list2, 3)).isEqualTo("no yes yes");
     }
+    @Test
+    public void 공식테스트solution2() {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(8,3,7,9,1));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(5,7,9));
+        Assertions.assertThat(ComponentSearch.solution2(list1, 5, list2, 3)).isEqualTo("no yes yes");
+    }
+
+    @Test
+    public void 임의테스트solution2() {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(8,3,7,9,1,2,6));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(5,7,9,6,11));
+        Assertions.assertThat(ComponentSearch.solution2(list1, 5, list2, 3)).isEqualTo("no yes yes yes no");
+    }
+
+    @Test
+    public void 중복테스트solution2() {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(8,3,3,4,1,4,6));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(5,7,3,6,4));
+        Assertions.assertThat(ComponentSearch.solution2(list1, 5, list2, 3)).isEqualTo("no no yes yes yes");
+    }
 
 
 }
