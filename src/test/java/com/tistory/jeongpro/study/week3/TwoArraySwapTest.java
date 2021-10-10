59b05e8c5bf0539642b7fd01f930193d87f5f7e3
dkjeong <ehdrylang@naver.com>
2021-10-10 14:37:43 +0900

    TC 간소화

--- src/test/java/com/tistory/jeongpro/study/week3/TwoArraySwapTest.java
+++ src/test/java/com/tistory/jeongpro/study/week3/TwoArraySwapTest.java
@@ -4,23 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class TwoArraySwapTest {
     @Test
     public void 공식테스트() {
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(5);
-        list1.add(4);
-        list1.add(3);
-        List<Integer> list2 = new ArrayList<>();
-        list2.add(5);
-        list2.add(5);
-        list2.add(6);
-        list2.add(6);
-        list2.add(5);
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,5,4,3));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(5,5,6,6,5));
         Assertions.assertThat(TwoArraySwap.solution(list1, list2, 3)).isEqualTo(26L);
     }
 
